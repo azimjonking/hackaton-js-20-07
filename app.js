61541c2968cbae9b219d7d0da3e0ee76261d16e8
AzimjonKing <105929226+azimjonking@users.noreply.github.com>
2024-07-20 12:13:21 +0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+// 1 - misol
+
+// 1 dan 100 gacha bo'lgan har bir raqamni yangi satrda chop etadigan qisqa dastur yozing. 
+
+// 3 ning har bir karrali uchun raqam o'rniga "Algo" ni chop eting. 
+
+// 5 ning har bir ko'paytmasi uchun raqam o'rniga "Ritm" ni chop eting. 
+
+// 3 va 5 ga karrali raqamlar uchun raqam o'rniga "AlgoRitm" ni chop eting.
+
+
+
+
+
+// 2 - misol
+
+// Sum Methodni for loopda yozib bering
+
+
+
+
+
+
+// dom uchun js code
